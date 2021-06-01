398bd2a8901aec01d2ec9ed9c4e99713953de663
Aleksandra <aleks.panf@gmail.com>
2021-06-01 20:10:36 +0300

    [feature/QA1RUSPR21-119]: Implement Calculator

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,24 @@
+public class Calculator {
+    //access-modifier
+    //data type
+    //add (method name)
+
+    //add
+    public double add(double firstNumber, double secondNumber) { return firstNumber + secondNumber;
+    }
+
+    //subtract
+    public double subtract(double firstNumber, double secondNumber) { return firstNumber - secondNumber;
+    }
+
+    //multiply
+
+    public double multiply(double firstNumber, double secondNumber) { return firstNumber * secondNumber;
+
+    }
+    //divide
+    public double divide(double firstNumber, double secondNumber) { return firstNumber / secondNumber;
+    }
+
+
+}
